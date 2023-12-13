df39d77da6180d9874eb642909ad4c5267ab06be
Gulbaha <64098871+gulbaha19@users.noreply.github.com>
2023-12-13 16:00:27 +0600

    cahnged name

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-app.use(express.static(__dirname + "/dist/midterm"));
+app.use(express.static(__dirname + "/dist/web"));
 app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname + "/dist/midterm/index.html"));
+  res.sendFile(path.join(__dirname + "/dist/web/index.html"));
 });
 
 app.listen(process.env.PORT || 8080);
